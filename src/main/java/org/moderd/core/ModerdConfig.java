a72d06da056de2e0a849f9b885cfb31c2edbb9f6
ansehyeong <dkstpgud@gmail.com>
2023-02-27 19:16:18 +0900

    feat: spring boot auto configuration properties 상속 ModelConfig 및 Parameters 추가

---
+++ src/main/java/org/moderd/core/ModerdConfig.java
@@ -0,0 +1,7 @@
+package org.moderd.core;
+
+import java.util.Properties;
+
+public class ModerdConfig extends Properties {
+    private static final long serialVersionUID = 2547234523462546L;
+}
---
+++ src/main/java/org/moderd/core/ModerdConfigParams.java
@@ -0,0 +1,5 @@
+package org.moderd.core;
+
+public class ModerdConfigParams {
+    public static final String BASE_PACKAGE = "base.package";
+}
